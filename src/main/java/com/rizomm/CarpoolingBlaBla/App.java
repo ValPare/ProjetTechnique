5ce482a14b9e358e40fdfa3d279b4177adaf814f
valentin <valentin@PC-de-Valentin>
2015-02-19 13:31:27 +0100

    1er commit

---
+++ src/main/java/com/rizomm/CarpoolingBlaBla/App.java
@@ -0,0 +1,13 @@
+package com.rizomm.CarpoolingBlaBla;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
